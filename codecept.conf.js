e1c1cbc7791fb374d1e1f4de58f242286001a62f
phuctran0811 <phuctran0811@gmail.com>
2025-10-27 14:15:34 +0700

    Complete CI/CD pipeline fix: simplify config, remove problematic dependencies, create working pipeline

--- codecept.conf.js
+++ codecept.conf.js
@@ -9,21 +9,5 @@ exports.config = {
   include: {
     I: './steps_file.js'
   },
-  plugins: {
-    allure: {
-      enabled: true,
-      require: '@codeceptjs/allure-legacy',
-    },
-    stepByStepReport: {
-      enabled: true,
-      screenshotsForAllureReport: true,
-    }
-  },
-  multiple: {
-    parallel: {
-      chunks: 2,
-      browsers: ['REST']
-    }
-  },
   name: 'test-api'
 }
